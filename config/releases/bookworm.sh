d5ec4a77ecaec1ae7d5dfd2e8dc4b6dfcd89e7d2
Theo M <github@theom.nz>
2024-05-07 11:50:04 +1200

    changes codenames

--- config/releases/bookworm.sh
+++ config/releases/bookworm.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
-export RELASE_NAME="Ubuntu 22.04 LTS (Jammy Jellyfish)"
-export RELASE_VERSION="22.04"
+export RELASE_NAME="Debian GNU/Linux 12 (bookworm)"
+export RELASE_VERSION="12"
 if [ -z "${KERNEL_TARGET}" ]; then
     export KERNEL_TARGET="rockchip-5.10"
 fi
--- config/releases/trixie.sh
+++ config/releases/trixie.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
-export RELASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
-export RELASE_VERSION="24.04"
+export RELASE_NAME="Debian GNU/Linux 13 (trixie)"
+export RELASE_VERSION="13"
 if [ -z "${KERNEL_TARGET}" ]; then
     export KERNEL_TARGET="rockchip-6.1"
 fi

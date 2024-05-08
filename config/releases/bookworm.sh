4b39886f4c6a1231c3296535fe7abf02d2106fd9
Theo M <github@theom.nz>
2024-05-08 21:38:07 +1200

    switch to codename

--- config/releases/bookworm.sh
+++ config/releases/bookworm.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
 export RELASE_NAME="Debian GNU/Linux 12 (bookworm)"
-export RELASE_VERSION="12"
+export RELEASE_VERSION="bookworm"
 if [ -z "${KERNEL_TARGET}" ]; then
     export KERNEL_TARGET="rockchip-5.10"
 fi
--- config/releases/trixie.sh
+++ config/releases/trixie.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
 export RELASE_NAME="Debian GNU/Linux 13 (trixie)"
-export RELASE_VERSION="13"
+export RELEASE_VERSION="trixie"
 if [ -z "${KERNEL_TARGET}" ]; then
     export KERNEL_TARGET="rockchip-6.1"
 fi

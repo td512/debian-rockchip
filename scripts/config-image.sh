c0ffe04f1631d32dc4db8cf3c4e94f7970d1527d
Theo Morra <github@theom.nz>
2024-05-12 00:35:35 +1200

    fix file paths again

--- scripts/config-image.sh
+++ scripts/config-image.sh
@@ -110,7 +110,7 @@ else
     DESTNAME="debian-${RELEASE_VERSION}-${DESKTOP}-preinstalled-${PROJECT}-arm64.rootfs"
 fi
 
-tar -xpJf "${DESTNAME}/${DESTNAME}.tar.xz" -C ${chroot_dir}
+tar -xpJf "${DESTNAME}.tar.xz/${DESTNAME}.tar.xz" -C ${chroot_dir}
 
 # Mount the root filesystem
 setup_mountpoint $chroot_dir
